0f6f946881afde3bbe332d536695e99b8cfb5598
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-06-16 22:01:04 +0200

    function calculate_VaR

---
+++ R/calculate_VaR.R
@@ -0,0 +1,58 @@
+#' Calculate Value at Risk (VaR) and Expected Shortfall (ES) of a portfolio
+#'
+#' This function calculates the Value at Risk (VaR) and Expected Shortfall (ES) using
+#' one of the three methods: historical, gaussian (parametric), and t-student.
+#'
+#' @param portfolio_returns A numeric vector of portfolio returns.
+#' @param alpha A numeric value between 0 and 1 representing the significance level.
+#' @param method A character string specifying the method to be used ('historical', 'gaussian', or 't-student'). Default is 'historical'.
+#'
+#' @return A list containing the VaR, ES, Realized Exceedances and Expected Exceedances.
+#' @export
+#'
+#' @examples
+#' # Example with historical method
+#' returns <- rnorm(1000, mean=0.0005, sd=0.01)
+#' calculate_VaR(returns, alpha=0.05, method="historical")
+#'
+#' # Example with gaussian method
+#' calculate_VaR(returns, alpha=0.05, method="gaussian")
+#'
+#' # Example with t-student method
+#' calculate_VaR(returns, alpha=0.05, method="t-student")
+
+calculate_VaR <- function(portfolio_returns, alpha = 0.05, method = "historical") {
+  # Input validation
+  if (!is.numeric(portfolio_returns)) {
+    stop("portfolio_returns should be a numeric vector.")
+  }
+  
+  if (length(portfolio_returns) < 2) {
+    stop("portfolio_returns should contain at least two data points.")
+  }
+  
+  if (!is.numeric(alpha) || alpha <= 0 || alpha >= 1) {
+    stop("alpha should be a numeric value between 0 and 1.")
+  }
+  
+  if (!is.character(method) || !method %in% c("historical", "gaussian", "t-student")) {
+    stop("Invalid method. Choose 'historical', 'gaussian', or 't-student'.")
+  }
+  
+  n = length(portfolio_returns)
+  
+  # Call the appropriate calculation function
+  results <- switch(
+    method
+    , historical = calculate_VaR_historical(portfolio_returns, alpha)
+    , gaussian = calculate_VaR_gaussian(portfolio_returns, alpha)
+    , `t-student` = calculate_VaR_t_student(portfolio_returns, alpha)
+    , stop("Invalid method. Choose 'historical', 'gaussian', or 't-student'.")
+  )
+  
+  # Add expected exceeds to results
+  results$Exceeds_Expected = alpha * n
+  
+  # Return results
+  return(results)
+}
