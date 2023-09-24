2b52c43863c98035e93469fb04d27dcf7a0d9b35
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-09-24 13:24:45 +0200

    generate formulas - higher polynomials + check degrees

--- R/generate_formula.R
+++ R/generate_formula.R
@@ -1,5 +1,9 @@
 generate_formula = function(y, xs, degrees, interaction_terms = NULL) {
   
+  if (!is.numeric(degrees) | !all(degrees > 0) | !all(round(degrees, 0) == degrees)) {
+    stop("Degrees must be numeric vector of positive integers.")
+  }
+  
   if (length(xs) != length(degrees)) {
     stop('Length of xs must me equal to the length of degrees.')
   }
@@ -8,7 +12,13 @@ generate_formula = function(y, xs, degrees, interaction_terms = NULL) {
   terms = c()
   
   for (i in 1:n_terms) {
-    terms = c(terms, xs[i])
+    for (j in 1:degrees[i]) {
+      if (j == 1) {
+        terms = c(terms, xs[i])
+      } else {            
+        terms = c(terms, paste0('I(', xs[i], '^', j, ')'))
+      }
+    }
   }
   
   paste0(y, ' ~ ', paste(terms, collapse = ' + '))
--- tests/testthat/test-generate_formula.R
+++ tests/testthat/test-generate_formula.R
@@ -4,10 +4,31 @@ test_that("Error where length of xs and degrees differs", {
   expect_error(generate_formula('y', xs, d))
 })
 
+test_that("Error when degrees is not numeric vector of positive integers", {
+  xs <- c('x1', 'x2')
+  d1 <- c(1, -1)
+  expect_error(generate_formula('y', xs, d1))
+  
+  d2 <- c(1, 0)
+  expect_error(generate_formula('y', xs, d2))
+  
+  d3 <- c('1', '1')
+  expect_error(generate_formula('y', xs, d3))
+  
+  d4 <- c(1.1, 1)
+  expect_error(generate_formula('y', xs, d4))
+})
 
 test_that("Function works with 1-degree polynomials", {
   xs <- c('x1', 'x2')
   d <- c(1, 1)
   f <- generate_formula('y', xs, d)
   expect_equal(f, 'y ~ x1 + x2')
+})
+
+test_that("Function works with higher degree polynomials", {
+  xs <- c('x1', 'x2')
+  d <- c(2, 3)
+  f <- generate_formula('y', xs, d)
+  expect_equal(f, 'y ~ x1 + I(x1^2) + x2 + I(x2^2) + I(x2^3)')
 })
